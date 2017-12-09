52f09d8491288fc5c76e2fddf7e5aa4f834b1ea2
pranavkulkarni <pranav.sk.90@gmail.com>
2017-12-09 16:07:43 -0500

    supporting a different kubeconfig location because docker containers do not share state at ~/.kube/config

--- commands/lib/kubecfg.js
+++ commands/lib/kubecfg.js
@@ -3,23 +3,22 @@ const fs = require('fs')
 const { kubectl } = require('../../')
 
 async function write (options = {}) {
-  /*
-    options = {
-      cluster: foo-bar.dev.koopernetes.com,
-      certificate-authority-data: 'base64 encoded cert auth data'
-    }
-  */
   await kubectl.config('set-cluster', options)
   await kubectl.config('set-context', options)
   await kubectl.config('use-context', options)
-  const kubeConfig = yaml.safeLoad(fs.readFileSync(`${process.env.HOME}/.kube/config`))
+  let kubeconfigLocation
+  if (process.env.KUBECONFIG) {
+    kubeconfigLocation = process.env.KUBECONFIG
+  } else {
+    kubeconfigLocation = `${process.env.HOME}/.kube/config`
+  }
+  const kubeConfig = yaml.safeLoad(fs.readFileSync(kubeconfigLocation))
   kubeConfig.clusters.forEach((clusterObj, i) => {
     if (clusterObj.name === options.cluster) {
       clusterObj.cluster['certificate-authority-data'] = options['certificate-authority-data']
     }
   })
-  console.log(kubeConfig)
-  fs.writeFileSync(`${process.env.HOME}/.kube/config`, yaml.safeDump(kubeConfig))
+  fs.writeFileSync(kubeconfigLocation, yaml.safeDump(kubeConfig))
 }
 
 module.exports = {
