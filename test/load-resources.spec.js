7c891b5b176728372b69656c7477b08ea41ad77a
pranavkulkarni <pranav.sk.90@gmail.com>
2018-05-24 15:19:32 -0400

    removed rootIsSpecificFile option

--- test/load-resources.spec.js
+++ test/load-resources.spec.js
@@ -22,7 +22,7 @@ test('load a list', async function (t) {
 
 test('load a specific file', async function (t) {
   t.plan(3)
-  const resources = await loadResources('test/fixtures/namespaces/test/cluster-autoscaler.hbs', { minSize: 1, maxSize: 10, env: 'dev-east-1', clusterDomain: 'foo.bar.com' }, { rootIsSpecificFile: true })
+  const resources = await loadResources('test/fixtures/namespaces/test/cluster-autoscaler.hbs', { minSize: 1, maxSize: 10, env: 'dev-east-1', clusterDomain: 'foo.bar.com' })
   t.equal(resources.length, 1, 'loaded correct number of resources')
   const resource = resources[0]
   t.equal(resource.kind, 'Deployment', 'loaded resource from template')
